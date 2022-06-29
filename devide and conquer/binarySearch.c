5f9f7dd92453bf04d433bb775c9c1adba53a159c
sharmaachintya08 <sharmachintya08@gmail.com>
2022-06-29 21:14:19 +0530

    bs updated

--- devide and conquer/binarySearch.c
+++ devide and conquer/binarySearch.c
@@ -1,35 +1,39 @@
 //time complexity of binary search in O(logn)
 //binary search is a searching algorithm
+//get it checked 
 #include <stdio.h>
-#define n 7
 
-int binarySearch(int a[],int beg,int end,int val){
-    int mid;
-    if(end>=beg){
-        mid=(beg+end)/2;
-        if(a[mid]==val){
-            return mid+1;
+int BinarySearch(int array[],int low,int high,int key);
+int main(){
+    int array[]={1,10,50,12,25,50,77};
+    int n = sizeof(array)/sizeof(array[0]);
+    int low = 0;
+    printf("type the element you want to search \n");
+    int key = scanf("%d",&key);
+    int index = BinarySearch(array,low,n-1,key);
+    if(index==-1){
+        printf("\n key element not found \n");
+    }
+    else{
+        printf("\nindex :- %d\n",index);
+    }
+    return 0;
+}
+int BinarySearch(int array[],int low,int high,int key){
+    int mid = (low+high)/2;
+    if(low==high){
+        return low;
+    }
+    else{
+        if(key<array[mid]){
+            BinarySearch(array,low,mid,key);
         }
-        else if(a[mid]<val){//this means the value is greater than the mid value 
-            return binarySearch(a,mid+1,end,val);
+        else if(key>array[mid]){
+            BinarySearch(array,mid+1,high,key);
         }
         else{
-            return binarySearch(a,beg,mid-1,val);
+            return mid;
         }
     }
     return -1;
-}
-int main(){
-    int array[]={1,2,3,4,5,6,7},val;
-    int res;
-    printf("type the value to be searched : \n");
-    scanf("%d",&val);
-    res = binarySearch(array,0,n-1,val);
-    if(res==-1){
-        printf("the element is not present in the array \n");
-    }
-    else{
-        printf("element is present at : %d",res);
-        printf("\n");
-    }
 }
