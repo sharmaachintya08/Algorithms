3942a72e765e2945cf97ae82f17151d3de2a33a8
Achintya <sharmaachintya08@gmail.com>
2022-07-24 00:56:38 +0530

    solved problem Given an integer array, find the smallest window sorting which will make the entire array sorted in increasing order , by window technique

---
+++ Optimized/WindowSort.java
@@ -0,0 +1,36 @@
+/**
+ * problem :- Smallest window in the array which can sort all of the array , using window technique; 
+ */
+
+class WindowSort{
+    public static void windowSort(int[] A){
+        int leftIndex = -1 , rightIndex = -1;
+        int max_so_far = Integer.MIN_VALUE;
+        for(int i=0;i<A.length;i++){
+            if(max_so_far < A[i]){
+                max_so_far = A[i];
+            }
+            if(A[i]<max_so_far){
+                rightIndex = i;
+            }
+        }
+        int min_so_far = Integer.MAX_VALUE;
+        for(int i = A.length - 1;i>=0;i--){
+            if(min_so_far > A[i]){
+                min_so_far = A[i];
+            }
+            if(A[i]>min_so_far){
+                leftIndex = i;
+            }
+        }
+        if(leftIndex == -1){
+            System.out.println("Array already sorted ");
+            return ;
+        }
+        System.out.println("the unsorted shortest array is from " + leftIndex + " to " + rightIndex);
+    }
+    public static void main(String[] args){
+        int[] A = {1,2,3,6,0,5,8,7,9};
+        windowSort(A);
+    }
+}
