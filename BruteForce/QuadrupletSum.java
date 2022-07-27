75563cd8d599749f3c5a3ea5cd73582604339465
Achintya <sharmaachintya08@gmail.com>
2022-07-27 19:52:53 +0530

    problem :- 4–Sum Problem | Quadruplets with a given sum , bruteforce

---
+++ BruteForce/QuadrupletSum.java
@@ -0,0 +1,34 @@
+/*
+ * problem :- 4–Sum Problem | Quadruplets with a given sum
+ * problem set:- 4-sum problem: Given an unsorted integer array, check if it contains four elements tuple (quadruplets) having a given sum.
+ */
+
+public class QuadrupletSum {
+    public static boolean hasQuadruplet(int[] nums,int n,int target,int count){
+        if (target == 0 && count == 4) {
+            return true;
+        }
+ 
+        // return false if the sum is not possible with the current configuration
+        if (count > 4 || n == 0) {
+            return false;
+        }
+ 
+        // Recur with
+        // 1. Including the current element
+        // 2. Excluding the current element
+ 
+        return hasQuadruplet(nums, n - 1, target - nums[n - 1], count + 1) ||
+                        hasQuadruplet(nums, n - 1, target, count);
+    }
+    public static void main(String[] args){
+        int[] nums = {2,7,4,0,9,1,3};
+        int target = 20;
+        if(hasQuadruplet(nums,nums.length,target,0)){
+            System.out.println("quadruplet exists");
+        }
+        else{
+            System.out.println("Quadruplet does not exist");
+        }
+    }
+}
