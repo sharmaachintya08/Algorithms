eff121ffdf6b1da6dbf98b0340272585f1ee6af8
Achintya <sharmaachintya08@gmail.com>
2022-07-30 14:21:34 +0530

    problem solved Find the minimum difference between the index of two given elements present in an array , using pointers

--- Hashing/ElementsOccurance.java
+++ Hashing/ElementsOccurance.java
@@ -7,23 +7,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ElementsOccurance {
-    public static void occurance(int[] A){
-        Map<Integer,Integer> freq = new HashMap<>();
-        for(int i : A){
-            freq.put(i,freq.getOrDefault(i,0)+1);
+    public static void occurance(int[] A) {
+        Map<Integer, Integer> freq = new HashMap<>();
+        for (int i : A) {
+            freq.put(i, freq.getOrDefault(i, 0) + 1);
         }
-        for(int i:A){
-            if(freq.containsKey(i)){
+        for (int i : A) {
+            if (freq.containsKey(i)) {
                 int n = freq.get(i);
-                while(n-- != 0){
-                    System.out.println(i+"");
+                while (n-- != 0) {
+                    System.out.println(i + "");
                 }
             }
             freq.remove(i);
         }
     }
-    public static void main(String[] args){
-        int[] A = {1,2,3,1,2,1};
+
+    public static void main(String[] args) {
+        int[] A = { 1, 2, 3, 1, 2, 1 };
         occurance(A);
     }
 }
