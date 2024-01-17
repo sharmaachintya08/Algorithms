86141922a2942f5342a42783653d73bdb336b541
achintya <sharmaachintya08@gmail.com>
2024-01-17 22:14:32 +0530

    removed java algos

--- LeetCode/TrailingZeroes.java
+++
@@ -1,9 +0,0 @@
-//find the trailing zeroes in factorial;
-
-package Algorithms.MathematicsAndNumberTheory;
-
-public class TrailingZeroes {
-    public static void main(String[] args){
-        
-    }
-}
--- LeetCode/TreeNode.sql
+++
@@ -1,7 +0,0 @@
---Leetcode - 608 Tree Node
-
-select id,'Root' as type from tree t where p_id is null
-union
-select id,'Inner' as type from tree t where exists(select 1 from tree where p_id = t.id ) and p_id is not null
-union
-select id,'Leaf' as type from tree t where not exists(select 1 from tree where p_id = t.id )and p_id is not null
